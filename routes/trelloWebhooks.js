37283fe218b142f0327a02352b9001cd86afdfdd
cule219 <culaficstefan@gmail.com>
2019-07-30 21:42:30 +0200

    Trello Webhooks handling creating and updating cards

--- routes/trelloWebhooks.js
+++ routes/trelloWebhooks.js
@@ -7,31 +7,31 @@ const Module = require("../models/Module");
 const TrelloResponse = require("../models/TrelloResponse");
 
 router.post('/trelloCallback', (req, res) => {
-  TrelloResponse.findOneAndUpdate({"response.action.id": req.body.action.id},{response: req.body}, {upsert: true})//action: req.body.action
+  TrelloResponse.update({'response.action.id': req.body.action.id},{response: req.body}, {upsert: true})//action: req.body.action
   
   // action.type = createCard
   if(req.body.action.display.translationKey === "action_create_card") {
-    Card.create({
+    Card.update({id: req.body.action.data.card.id},{
         id: req.body.action.data.card.id, 
         name: req.body.action.data.card.name, 
         shortLink: req.body.action.data.card.shortLink,
         idList: req.body.action.data.list.id
-    }).then(card => {
+    }, {upsert: true}).then(card => {
         console.log(card);
     }) 
   }
   // action.type = updateCard
   else if(req.body.action.display.translationKey === "action_archived_card") {
-    Card.findByIdAndDelete(req.body.action.data.card.id).then(data => 
+    Card.findOneAndDelete({id: req.body.action.data.card.id}).then(data => 
         console.log(data.length));
   }
   // action.type = addAttachmentToCard
   else if(req.body.action.display.translationKey === "action_add_attachment_to_card") {
-      Card.findByIdAndUpdate(req.body.action.data.card.id, {$push: {attachments: req.body.action.data.attachment.url}})
+      Card.findOneAndUpdate({id: req.body.action.data.card.id}, {$push: {attachments: req.body.action.data.attachment.url}})
   }
   // action.type = addLabelToCard
   else if(req.body.action.display.translationKey === "action_add_label_to_card") {
-      Card.findByIdAndUpdate(req.body.action.data.card.id, 
+      Card.findOneAndUpdate({id: req.body.action.data.card.id}, 
         {$push: 
             {tags: {
                 name: req.body.action.data.text,
