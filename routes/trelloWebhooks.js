5199a6aebfdce0769bef217df0cce5cf9d9d464a
cule219 <culaficstefan@gmail.com>
2019-07-30 14:21:17 +0200

    trello webhooks init

--- routes/trelloWebhooks.js
+++ routes/trelloWebhooks.js
@@ -4,8 +4,10 @@ const Card = require("../models/Card");
 const List = require("../models/List");
 const Board = require("../models/Board");
 const Module = require("../models/Module");
+const TrelloResponse = require("../models/TrelloResponse");
 
 router.post('/trelloCallback', (req, res) => {
-  console.log(req.body);
+  console.log(req.body)
+  TrelloResponse.create({response: req.body});
   res.status(200);
 })
